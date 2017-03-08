087207f191d02437bcb772eb812dafc9a9f70dbd
mdjcole <mdjcole@gmail.com>
2017-03-08 17:05:06 +0100

    First working example of animated plot

--- plot_fld/plot_fld.py
+++ plot_fld/plot_fld.py
@@ -1,35 +1,36 @@
 #script for reading binaryangy_x_y.fld files produced by EUTERPE
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as an
 
 #functions for reading fortran records - before and after each record is a 4 byte int
 #stating the length of that record in bytes, which must be omitted
-def ftread_string(name):
-    null = np.fromfile(file=name, dtype='>i4', count=1)
-    out = np.fromfile(file=name, dtype='i1', count=null[0])
-    null = np.fromfile(file=name, dtype='>i4', count=1)
+def ftread_string():
+    null = np.fromfile(file=f, dtype='>i4', count=1)
+    out = np.fromfile(file=f, dtype='i1', count=null[0])
+    null = np.fromfile(file=f, dtype='>i4', count=1)
     return out
 
-def ftread_double(name):
-    null = np.fromfile(file=name, dtype='>i4', count=1)
-    out = np.fromfile(file=name, dtype='>f8', count=null[0]//8)
-    null = np.fromfile(file=name, dtype='>i4', count=1)
+def ftread_double():
+    null = np.fromfile(file=f, dtype='>i4', count=1)
+    out = np.fromfile(file=f, dtype='>f8', count=null[0]//8)
+    null = np.fromfile(file=f, dtype='>i4', count=1)
     return out
 
-def ftread_int(name):
-    null = np.fromfile(file=name, dtype='>i4', count=1)
-    out = np.fromfile(file=name, dtype='>i4', count=null[0]//4)
-    null = np.fromfile(file=name, dtype='>i4', count=1)
+def ftread_int():
+    null = np.fromfile(file=f, dtype='>i4', count=1)
+    out = np.fromfile(file=f, dtype='>i4', count=null[0]//4)
+    null = np.fromfile(file=f, dtype='>i4', count=1)
     return out
 
-def ftread_longint(name):
-    null = np.fromfile(file=name, dtype='>i4', count=1)
-    out = np.fromfile(file=name, dtype='>i8', count=null[0]//8)
-    null = np.fromfile(file=name, dtype='>i4', count=1)
+def ftread_longint():
+    null = np.fromfile(file=f, dtype='>i4', count=1)
+    out = np.fromfile(file=f, dtype='>i8', count=null[0]//8)
+    null = np.fromfile(file=f, dtype='>i4', count=1)
     return out
 
-def fthead(name):
-    null = np.fromfile(file=name, dtype='>i4', count=1)
+def fthead():
+    null = np.fromfile(file=f, dtype='>i4', count=1)
 
 def cycread(f):
 #read cyclic data
@@ -54,13 +55,13 @@ s = 1
 f = open('prod_msdmp_angy_phi_pol.fld','rb')
 
 #read header
-codename=ftread_string(f)
-VERNUM=ftread_double(f)
-zverformat=ftread_double(f)
-NUM_INT_HEADER=ftread_int(f)
-int_header=ftread_longint(f)
-NUM_REAL_HEADER=ftread_int(f)
-zreal_header=ftread_double(f)
+codename=ftread_string()
+VERNUM=ftread_double()
+zverformat=ftread_double()
+NUM_INT_HEADER=ftread_int()
+int_header=ftread_longint()
+NUM_REAL_HEADER=ftread_int()
+zreal_header=ftread_double()
 
 #print(codename)
 #print(VERNUM)
@@ -76,22 +77,38 @@ if s:
 else:
   sgrid=np.arange(0.,1.,1./nums)**0.5
   
-#while 1:
+def cycdat():
 #read cyclic data
 #header
-fthead(f)
-simtime=np.fromfile(file=f, dtype='>f8', count=1)
-mmin_filter=np.fromfile(file=f, dtype='>i8', count=1)
-im_number=np.fromfile(file=f, dtype='>i8', count=1)
-fthead(f)
+    fthead()
+    simtime=np.fromfile(file=f, dtype='>f8', count=1)
+    mmin_filter=np.fromfile(file=f, dtype='>i8', count=1)
+    im_number=np.fromfile(file=f, dtype='>i8', count=1)
+    fthead()
 #body
-fthead(f)
-cfftlist=np.fromfile(file=f, dtype='>c16', count=nums*im_number[0])
-fthead(f)
+    fthead()
+    cfftlist=np.fromfile(file=f, dtype='>c16', count=nums*im_number[0])
+    fthead()
 #reshape into array (mode number, radial location)
-cfftdata=np.reshape(cfftlist,(128,33))  
-#assemble grids for plot
+    cfftdata=np.reshape(cfftlist,(128,33))  
+    return simtime, cfftdata
+
+#plt.plot(sgrid,phigrid)
+#plt.title(simtime)
+#plt.show()
+
+simtime, cfftdata=cycdat()
 phigrid=np.absolute(cfftdata[:,26])
-#plot data
-plt.plot(sgrid,phigrid)
-plt.show()
+
+plt.ion()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line1, = ax.plot(sgrid, phigrid, 'r-')
+
+for i in range(200):
+    line1.set_ydata(phigrid)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    simtime, cfftdata=cycdat()
+    phigrid=np.absolute(cfftdata[:,26])
